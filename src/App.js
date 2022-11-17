f95fb7c3558001a3895684cece1161cc48662114
Campos2002 <campereira91@gmail.com>
2022-11-17 02:38:28 -0300

    Personal Portfolio Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header'
+import Nav from './components/Nav'
+import About from './components/About'
+import Experience from './components/Experience'
+import Services from './components/Services'
+import Portfolio from './components/Portfolio'
+import Testimonials from './components/Testimonials'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
