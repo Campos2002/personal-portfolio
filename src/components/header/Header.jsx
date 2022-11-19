e90601cc21a1a41c1cba4a87d85fa33594871333
Campos2002 <campereira91@gmail.com>
2022-11-19 20:02:57 -0300

    Personal Portfolio Project

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,18 +9,18 @@ const Header = () => {
       <header>
 
         <div className="container header__container">
+
           <h5>Hello, I'm</h5>
           <h1>Guilherme Campos</h1>
           <h5 className="text-light">Front-end Developer</h5>
           <CTA />
           <HeaderSocials />
-        </div>
+          <div className="me">
+            <img src={ME} alt="me" />
+          </div>
+          <a href="#contact" className='scroll__down'>Scroll Down</a>
 
-        <div className="me">
-          <img src={ME} alt="me" />
         </div>
-
-        <a href="#contact" className='scroll__down'>Scroll Down</a>
 
       </header>
     </div>
