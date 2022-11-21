17d614c23999b71f1ae28773909bfea385b47fe6
Campos2002 <campereira91@gmail.com>
2022-11-21 02:58:19 -0300

    Personal Portfolio Project

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,60 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me-about.jpg'
+import { FaAward } from 'react-icons/fa'
+import { FiUsers } from 'react-icons/fi'
+import { VscFolderLibrary } from 'react-icons/vsc'
 
 const about = () => {
   return (
-    <section id='about'>About</section>
+
+    <section id='about'>
+
+      <h5>Get To know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+
+        <div className="about__content">
+
+          <div className="about__cards">
+
+            <article className='about__card'>
+              <FaAward className='about__icon' />
+              <h5>Experience</h5>
+              <small>1+ Year Working</small>
+            </article>
+
+            <article className='about__card'>
+              <FiUsers className='about__icon' />
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
+            </article>
+
+            <article className='about__card'>
+              <VscFolderLibrary className='about__icon' />
+              <h5>Projects</h5>
+              <small>80+ Completed</small>
+            </article>
+
+          </div>
+
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent fermentum quam sed venenatis tincidunt. Mauris posuere metus justo, eu feugiat urna hendrerit non. Integer sit amet fringilla dui. Proin at facilisis leo, vel lobortis lacus. Integer non viverra est, ut commodo neque. Nullam imperdiet volutpat magna, mollis cursus mi malesuada sed. Suspendisse varius faucibus nisl id egestas. In tincidunt, odio a ullamcorper sodales.
+          </p>
+
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+
+        </div>
+
+      </div>
+    </section>
   )
 }
 
