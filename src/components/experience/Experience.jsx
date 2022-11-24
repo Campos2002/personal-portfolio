15d243245757a4ea9808fd8a4d019cb5eafd33c7
Campos2002 <campereira91@gmail.com>
2022-11-23 23:51:27 -0300

    Personal Portfolio Project

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,12 +1,48 @@
 import React from 'react'
-
+import './experience.css'
+import { BsPatchCheckFill } from 'react-icons/bs'
 const Experience = () => {
-    return (
-        <section id='experience'>
-            <h5>Experience</h5>
-            <h2>Experience</h2>
-        </section>
-    )
+  return (
+    <section id='experience'>
+      <h5>Experience</h5>
+      <h2>Experience</h2>
+
+      <div className="container experience__container">
+        <div className="experience__frontende">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className='experience__details'>
+              <BsPatchCheckFill />
+              <h4>HTML</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill />
+              <h4>CSS</h4>
+              <small className='text-light'>Intermidiate</small>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill />
+              <h4>React</h4>
+              <small className='text-light'>Intermediate</small>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill />
+              <h4>JavaScript</h4>
+              <small className='text-light'>Intermediate</small>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill />
+              <h4>Git</h4>
+              <small className='text-light'>Intermediate</small>
+            </article>
+          </div>
+        </div>
+
+      </div>
+
+    </section>
+  )
 }
 
-export default Experience
+export default Experience;
