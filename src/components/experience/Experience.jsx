a9d3d74aa36d45fbda0903209794ce1c9f616bd6
Campos2002 <campereira91@gmail.com>
2022-11-23 23:42:11 -0300

    Personal Portfolio Project

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-const experience = () => {
-  return (
-    <section id='experience'>
-      <h5>Experience</h5>
-      <h2>Experience</h2>
-    </section>
-  )
+const Experience = () => {
+    return (
+        <section id='experience'>
+            <h5>Experience</h5>
+            <h2>Experience</h2>
+        </section>
+    )
 }
 
-export default experience
+export default Experience
