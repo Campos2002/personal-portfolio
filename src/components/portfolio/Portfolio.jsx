60eaff98d777894115f86d08a644d2c068013554
Campos2002 <campereira91@gmail.com>
2022-11-28 12:06:23 -0300

    Personal Portfolio Project

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -7,6 +7,45 @@ import IMG4 from '../../assets/portfolio4.jpg'
 import IMG5 from '../../assets/portfolio5.png'
 import IMG6 from '../../assets/portfolio6.jpg'
 
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Secrete Word',
+    github: 'https://github.com/Campos2002/secret-word-game',
+    demo: 'https://campos2002.github.io/secret-word-game/'
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'BMI Calculator',
+    github: 'https://github.com/Campos2002/calc_de_IMC',
+    demo: 'https://campos2002.github.io/calc_de_IMC/'
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'Bank Landing Page',
+    github: 'https://github.com/Campos2002/Virtual-Bank-Landing-Page',
+    demo: 'https://campos2002.github.io/Virtual-Bank-Landing-Page/'
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: 'Netflix Hompage Clone',
+    github: 'https://github.com/Campos2002/Clone-Netflix-Landing-Page',
+    demo: 'https://campos2002.github.io/Clone-Netflix-Landing-Page/'
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title: 'Spotify Homepage Clone',
+    github: 'https://github.com/Campos2002/Clone-Spotify-Landing-Page',
+    demo: 'https://campos2002.github.io/Clone-Spotify-Landing-Page/'
+  },
+];
+
 const Portfolio = () => {
   return (
     <section id='portfolio'>
@@ -19,65 +58,10 @@ const Portfolio = () => {
           <div className="portfolio__item-image">
             <img src={IMG1} alt="" />
           </div>
-          <h3>This is a portfolio item title</h3>
+          <h3>Secret Word</h3>
           <div className="portfolio__item-cta">
-            <a href="https://campos2002.github.io/secret-word-game/" className='btn' target='_blank'>Secret Word</a>
-            <a href="https://campos2002.github.io/calc_de_IMC/" className='btn btn-primary' target='_blank'>BMI Calculator</a>
-          </div>
-        </article>
-
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={IMG2} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a href="https://campos2002.github.io/secret-word-game/" className='btn' target='_blank'>Secret Word</a>
-            <a href="https://campos2002.github.io/calc_de_IMC/" className='btn btn-primary' target='_blank'>BMI Calculator</a>
-          </div>
-        </article>
-
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={IMG3} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a href="https://campos2002.github.io/secret-word-game/" className='btn' target='_blank'>Secret Word</a>
-            <a href="https://campos2002.github.io/calc_de_IMC/" className='btn btn-primary' target='_blank'>BMI Calculator</a>
-          </div>
-        </article>
-
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={IMG4} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a href="https://campos2002.github.io/secret-word-game/" className='btn' target='_blank'>Secret Word</a>
-            <a href="https://campos2002.github.io/calc_de_IMC/" className='btn btn-primary' target='_blank'>BMI Calculator</a>
-          </div>
-        </article>
-
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={IMG5} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a href="https://campos2002.github.io/secret-word-game/" className='btn' target='_blank'>Secret Word</a>
-            <a href="https://campos2002.github.io/calc_de_IMC/" className='btn btn-primary' target='_blank'>BMI Calculator</a>
-          </div>
-        </article>
-
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={IMG6} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a href="https://campos2002.github.io/secret-word-game/" className='btn' target='_blank'>Secret Word</a>
-            <a href="https://campos2002.github.io/calc_de_IMC/" className='btn btn-primary' target='_blank'>BMI Calculator</a>
+            <a href="https://github.com/Campos2002/secret-word-game" className='btn' target='_blank' rel='noreferrer'>GitHub</a>
+            <a href="https://campos2002.github.io/secret-word-game/" className='btn btn-primary' target='_blank' rel='noreferrer'>Demo</a>
           </div>
         </article>
 
