d4cb73f4e3c335233d1e95325f40093862640102
Campos2002 <campereira91@gmail.com>
2022-11-28 21:38:52 -0300

    Personal Portfolio Project

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,56 +1,6 @@
 import React from 'react'
 import './portfolio.css'
-import IMG1 from '../../assets/portfolio1.jpg'
-import IMG2 from '../../assets/portfolio2.jpg'
-import IMG3 from '../../assets/portfolio3.jpg'
-import IMG4 from '../../assets/portfolio4.jpg'
-import IMG5 from '../../assets/portfolio5.png'
-import IMG6 from '../../assets/portfolio6.jpg'
-
-const data = [
-  {
-    id: 1,
-    image: IMG1,
-    title: 'Secrete Word',
-    github: 'https://github.com/Campos2002/secret-word-game',
-    demo: 'https://campos2002.github.io/secret-word-game/'
-  },
-  {
-    id: 2,
-    image: IMG2,
-    title: 'BMI Calculator',
-    github: 'https://github.com/Campos2002/calc_de_IMC',
-    demo: 'https://campos2002.github.io/calc_de_IMC/'
-  },
-  {
-    id: 3,
-    image: IMG3,
-    title: 'Bank Landing Page',
-    github: 'https://github.com/Campos2002/Virtual-Bank-Landing-Page',
-    demo: 'https://campos2002.github.io/Virtual-Bank-Landing-Page/'
-  },
-  {
-    id: 4,
-    image: IMG4,
-    title: 'Netflix Hompage Clone',
-    github: 'https://github.com/Campos2002/Clone-Netflix-Landing-Page',
-    demo: 'https://campos2002.github.io/Clone-Netflix-Landing-Page/'
-  },
-  {
-    id: 5,
-    image: IMG5,
-    title: 'Spotify Homepage Clone',
-    github: 'https://github.com/Campos2002/Clone-Spotify-Landing-Page',
-    demo: 'https://campos2002.github.io/Clone-Spotify-Landing-Page/'
-  },
-  {
-    id: 6,
-    image: IMG6,
-    title: 'Museum Website',
-    github: 'https://github.com/Campos2002/Museum-Website-Project',
-    demo: 'https://campos2002.github.io/Museum-Website-Project/'
-  },
-];
+import data from '../portfolio/data'
 
 const Portfolio = () => {
   return (
