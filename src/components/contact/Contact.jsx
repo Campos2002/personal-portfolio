623f008c878fe2472dc12aabb40613982b43c447
Campos2002 <campereira91@gmail.com>
2022-11-30 00:34:04 -0300

    Personal Portfolio Project

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -10,28 +10,35 @@ const contact = () => {
       <h5>Get In Touch</h5>
       <h2>Contact Me</h2>
       <div className="container contact__container">
+        {/* Contact Options */}
         <div className="contact__options">
           <article className="contact__option">
-            <MdOutlineEmail />
+            <MdOutlineEmail className='contact__option-icon' />
             <h4>E-mail</h4>
             <h5>campereira@gmail.com</h5>
-            <a href="mailto:campereira91@gmail.com">Send a message</a>
+            <a href="mailto:campereira91@gmail.com" target="_blank" rel="noreferrer">Send a message</a>
           </article>
 
           <article className="contact__option">
-            <RiMessengerLine />
+            <RiMessengerLine className='contact__option-icon' />
             <h4>Messenger</h4>
             <h5>Guilherme Campos</h5>
             <a href="https://m.me/guilherme.campos.1991" target="_blank" rel="noreferrer">Send a message</a>
           </article>
 
           <article className="contact__option">
-            <BsWhatsapp />
+            <BsWhatsapp className='contact__option-icon' />
             <h4>WhatsApp</h4>
             <a href="https://api.whatsapp.com/send?phone=5521994008518" target="_blank" rel="noreferrer">Let's Talk</a>
           </article>
-
         </div>
+        {/* Form */}
+        <form action="">
+          <input type="text" name='name' placeholder='Yor Full Name' required />
+          <input type="email" name='email' placeholder='Your E-mail' required />
+          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
       </div>
     </section>
   )
