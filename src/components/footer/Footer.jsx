d340420d7ee0c82ec2b629594fa773c26a56c378
Campos2002 <campereira91@gmail.com>
2022-12-01 22:15:42 -0300

    modificação da paleta de cores e algumas classes no css relacionadas as paletas

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
       <ul className='permalinks'>
         <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
-        <li><a href="experience">Experience</a></li>
+        <li><a href="#experience">Experience</a></li>
         {/* <li><a href="#services">Services</a></li> */}
         <li><a href="#portfolio">Portfolio</a></li>
         <li><a href="#testimonials">Testimonials</a></li>
