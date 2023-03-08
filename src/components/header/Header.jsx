7b43549c7067f8e94642ba6de282af83657c0a2d
Campos2002 <campereira91@gmail.com>
2023-03-08 16:41:52 -0300

    edição do component Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -16,7 +16,6 @@ const Header = () => {
           <div className="me">
             <img src={ME} alt="me" />
           </div>
-          <a href="#contact" className='scroll__down'>Scroll Down</a>
 
         </div>
 
