75be0bf50b10cbbcb4140c912e4b64158747148a
Campos2002 <campereira91@gmail.com>
2023-07-29 17:13:36 -0300

    alteração de curriculo

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import CV from '../../assets/CTA/luiz_guilherme_cv.pdf'
+import CV from '../../assets/CTA/Luiz_Guilherme_Campos-Curriculo.pdf'
 
 const CTA = () => {
   return (
